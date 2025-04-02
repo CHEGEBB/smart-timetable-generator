3737f45de090396e65df3872749b849fa6c67152
CHEGEBB <chegephil24@gmail.com>
2025-04-02 09:27:24 +0300

    Refactor API URL to use the production environment

--- frontend/services/authService.js
+++ frontend/services/authService.js
@@ -1,5 +1,5 @@
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://smart-timetable-generator-1.onrender.com/api';
+// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://smart-timetable-generator-1.onrender.com/api';
 
 // Register user
 export const registerUser = async (userData) => {
